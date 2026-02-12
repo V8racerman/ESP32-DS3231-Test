0e8f625e67e89d8ff60dc190ef14f0fdc46a3e6c
V8racerman <v8racerman@gmail.com>
2026-02-12 16:00:07 +0000

    Removed redundant code (eg some searialprint debug code)

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,16 @@
 #include <Wire.h>
 #include <SPI.h>
 
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
 #include "setgpio.h"
 #include "SimpleAlarmClock.h"
 #include "Clocks.h"
 #include "TimeUtils.h"
 #include "clock_constants.h"
-#include "my_serial.h"
 #include "myOLED.h"
 #include "myLED.h"
 #include "Tones.h"
-
-const int flashInterval = 500;         // Alarm flashing interval
-
-bool pmFlag, h12Flag, century = false;
-byte alarmDay, alarmHour, alarmMinute, alarmSecond, alarmBits;
-bool alarmDy, alarmH12Flag, alarmPmFlag;
 
 SimpleAlarmClock extern Clock;
 SimpleAlarmClock Clock(RTC_addr, EEPROM_addr, INTCN);
@@ -29,16 +22,11 @@ DateTime NowTime;
 AlarmTime myAlarm1, myAlarm2;
 byte previous_second = 0;
 byte previous_minute = 0;
-byte          cpIndex = 0;              // Cursor Position Index - used for edit mode
-byte          ActiveAlarms = 0;         // used to store active alarms
+// byte          cpIndex = 0;              // Cursor Position Index - used for edit mode
 bool          changeFlag = true;
 
-States ClockState;
-States PrevState;
-
-// int nx = 0;
-// int melody_index = 0;
-// int loop_counter = 0;
+States ClockState = PowerLoss;
+States PrevState = ShowClock;
 
 void setup() {
 
@@ -49,13 +37,12 @@ void setup() {
   pinMode(SELECT_SW, INPUT);
   pinMode(SQW_PIN, INPUT_PULLUP);
   
-
   Wire.begin();
   Serial.begin(115200);
   Setup_Tone();
   clearAlarms();
-  preset_Alarms(9,  0, 0, alarm1);
-  preset_Alarms(11, 0, 0, alarm2);
+  preset_Alarms(10,  0, 0, alarm1);
+  preset_Alarms(17, 50, 0, alarm2);
   Update_temperature(&temperature);
   NowTime = Clock.read();
   myAlarm1 = Clock.readAlarm(alarm1);
@@ -82,19 +69,14 @@ void loop() {
       }
     }
     } else { 
-    changeFlag = Update_Clock(&NowTime, &myAlarm1, &myAlarm2, &temperature);
-    if (changeFlag) {
-      changeFlag = false;   
-      my_serial_time(NowTime);
-      Update_Display(&NowTime, &myAlarm1, &myAlarm2, temperature);
-      // my_serial_monitor(NowTime, h12Flag, pmFlag, true);
-      // my_serial_time(NowTime);
-      segment7_time(NowTime.Hour, NowTime.Minute, true);
-      delay(500);
-      segment7_time(NowTime.Hour, NowTime.Minute, false);
-      if (NowTime.Hour == 18 && NowTime.Minute == 55  && NowTime.Second == 0) { internet_time(); }
-      previous_minute = NowTime.Minute;
-      previous_second = NowTime.Second;
+      if (Update_Clock(&NowTime, &myAlarm1, &myAlarm2, &temperature)) {
+        Update_Display(&NowTime, &myAlarm1, &myAlarm2, temperature);
+        previous_minute = NowTime.Minute;
+        previous_second = NowTime.Second;
+        segment7_time(NowTime.Hour, NowTime.Minute, true);
+        delay(500);
+        segment7_time(NowTime.Hour, NowTime.Minute, false);
+        if (NowTime.Hour == 2 && NowTime.Minute == 5  && NowTime.Second == 30) { internet_time(); }
     }
   }
 }
